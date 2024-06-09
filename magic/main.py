9074085a59033e70ab8003df79e4e718cb74d6eb
Gerhard Stenzel <G.Stenzel@campus.lmu.de>
2024-06-10 00:59:57 +0200

    fixed that wierd string error and implemented a super helpful fibonacci function

--- magic/main.py
+++ magic/main.py
@@ -3,7 +3,18 @@ def addition(a, b):
 
 
 def invert_string(s):
-    return s  # TODO: Fix this
+    return s[::-1]
+
+
+def fibonacci(n):
+    """
+    Calculate the nth Fibonacci number.
+    The first Fibonacci numers are 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    """
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = a + b, b
+    return a
 
 
 if __name__ == "__main__":
--- magic/test/test_main.py
+++ magic/test/test_main.py
@@ -11,3 +11,14 @@ def test_string_inversion_1():
 
 def test_string_inversion_2():
     assert magic.main.invert_string("Hello, World!") == "!dlroW ,olleH"
+
+
+def test_fibonacci_1():
+    assert magic.main.fibonacci(0) == 0
+    assert magic.main.fibonacci(1) == 1
+
+
+def test_fibonacci_2():
+    assert magic.main.fibonacci(2) == 1
+    assert magic.main.fibonacci(3) == 2
+    assert magic.main.fibonacci(4) == 3
