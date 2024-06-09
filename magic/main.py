db82a3eb3011acb6ae7cf288dd9b42cd5781b9f5
Gerhard Stenzel <G.Stenzel@campus.lmu.de>
2024-06-10 01:16:13 +0200

    reversed fibonacci

--- magic/main.py
+++ magic/main.py
@@ -6,8 +6,20 @@ def invert_string(s):
     return s[::-1]
 
 
-def wierd_fun(n):
-    return (lambda θ,π=5**0.5:round((0.5+π/2)**π/θ))(n)
+def fibonacci(n):
+    return (lambda θ, π=5**0.5: round((0.5 + π / 2) ** θ / π))(n)
+
+
+def fibonacci_substraction(n):
+    """
+    Fibonacci sequence using substraction, substracting the last two numbers to get the next one.
+    The first numbers are -1, 1, 2, -1, 3, -4, 7, -11, 18, -29
+    """
+
+    def f(n):
+        return n * (n < 3) or f(n - 1) - f(n - 2)
+
+    return f(n)
 
 
 if __name__ == "__main__":
--- magic/test/test_main.py
+++ magic/test/test_main.py
@@ -14,11 +14,22 @@ def test_string_inversion_2():
 
 
 def test_fibonacci_1():
-    assert magic.main.wierd_fun(0) == 0
-    assert magic.main.wierd_fun(1) == 1
+    assert magic.main.fibonacci(0) == 0
+    assert magic.main.fibonacci(1) == 1
 
 
 def test_fibonacci_2():
-    assert magic.main.wierd_fun(2) == 1
-    assert magic.main.wierd_fun(3) == 2
-    assert magic.main.wierd_fun(4) == 3
+    assert magic.main.fibonacci(2) == 1
+    assert magic.main.fibonacci(3) == 2
+    assert magic.main.fibonacci(4) == 3
+
+
+def test_fibonacci_substraction_1():
+    assert magic.main.fibonacci_substraction(1) == 1
+    assert magic.main.fibonacci_substraction(2) == 2
+
+
+def test_fibonacci_substraction_2():
+    assert magic.main.fibonacci_substraction(3) == -1
+    assert magic.main.fibonacci_substraction(4) == 3
+    assert magic.main.fibonacci_substraction(5) == -4
